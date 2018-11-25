f46bb2bae2334e6f49a560254224f5ccfe731b32
tjsgtql <39507226+tjsgtql@users.noreply.github.com>
2018-11-25 22:00:41 +0800

    修改了输入
    商店下的一个四轴的模型，用的是standard asset里的控制。在2018里标准资源被移除了，所以重写了input部分

---
+++ HardFlying/Assets/_Script/MyInput.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyInput : MonoBehaviour
+{
+    [SerializeField] float RSpeed;
+    [SerializeField] float LSpeed;
+
+    public float Rx { get; private set; }
+    public float Ry { get; private set; }
+    public float Lx { get; private set; }
+    public float Ly { get; private set; }
+
+    void Update()
+    {
+        float deltaTime = Time.deltaTime;
+
+        Ry = Sensor(KeyCode.UpArrow, 1 * RSpeed, deltaTime, Ry);
+        Ry = Sensor(KeyCode.DownArrow, -1 * RSpeed, deltaTime, Ry);
+        Rx = Sensor(KeyCode.RightArrow, 1 * RSpeed, deltaTime, Rx);
+        Rx = Sensor(KeyCode.LeftArrow, -1 * RSpeed, deltaTime, Rx);
+
+        Ly = Sensor(KeyCode.W, 1 * LSpeed, deltaTime, Ly);
+        Ly = Sensor(KeyCode.S, -1 * LSpeed, deltaTime, Ly);
+        Lx = Sensor(KeyCode.D, 1 * RSpeed, deltaTime, Lx);
+        Lx = Sensor(KeyCode.A, -1 * LSpeed, deltaTime, Lx);
+    }
+
+    float Sensor(KeyCode keyCode, float speed, float timer, float senser)
+    {
+        if (Input.GetKey(keyCode))
+        {
+            senser += timer * speed;
+        }
+        else if (Input.GetKeyDown(keyCode))
+        {
+            senser = 0;
+        }
+        return senser;
+    }
+}
